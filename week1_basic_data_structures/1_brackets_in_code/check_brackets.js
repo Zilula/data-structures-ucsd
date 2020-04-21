ea2519511e91bd0c2b3bb52fdee506133979af4c
Lance Owen Merrill <lanceomerrill@gmail.com>
2020-04-21 09:57:39 -0700

    day one push

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.js
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.js
@@ -1,69 +1,40 @@
-class Stack {
-    constructor() {
-        this.list = [];
-    }
-    push(ele) {
-        return this.list.push(ele)
-    }
-    pop() {
-        return this.list.pop()
-    }
-    isEmpty() {
-        return this.list.length === 0
-    }
-    print() {
-      return this.list
-    }
-
-}
-
-
-class check_brackets {
-    main(data) {
-      const str = data.split('')
-        const stack = new Stack()
-
-        for(let i = 0; i < str.length; i ++) {
-            const char = str[i]
-            if(char === '{' || char === '[' || char === '(') {
-              stack.push(char)
-            }
-            else {
-                if (stack.isEmpty()) {
-                    return i + 1
-                }
-                const top = stack.pop()
-                if(
-                  (top === '[' && char !== ']') || 
-                  (top === '(' && char !== ')') || 
-                  (top === '{' && char !== '}') 
-                  ) {
-                    return i + 1
-                }
-            }
-        }
-
-        return 'Success'
-    }
-}
-var readline = require('readline');
-
-/*
- * We need to create a readline interface.
- * The interface takes 2 streams.
- * The input field points to the readable input stream
- * and the output field to the writable output stream. 
- */
-var rl = readline.createInterface({
+const readline = require('readline');
+const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    terminal: false
 });
 
-const hub = new check_brackets
+process.stdin.setEncoding('utf8');
 
-const result = hub.main(process.argv[1])
-console.log(result)
+rl.once('line', line => {
+    console.log(findMismatch(line));
+    process.exit();
+});
 
-console.log(rl)
+function areMatching(left, right) {
+    return ['{}', '()', '[]'].includes(left + right);
+}
 
-console.log(process.argv)
+function findMismatch(text) {
+    const openingBracketsStack = [];
+
+    for (let [idx, ch] of [...text].entries()) {
+        if ('{(['.includes(ch)) {
+            openingBracketsStack.push({ char: ch, position: idx + 1 });
+        }
+
+        if ('})]'.includes(ch)) {
+            if (openingBracketsStack.length === 0 || !areMatching(openingBracketsStack.pop().char, ch)) {
+                return idx + 1;
+            }
+        }
+    }
+
+    if (openingBracketsStack.length !== 0) {
+        return openingBracketsStack.pop().position;
+    }
+
+    return 'Success';
+}
+
+module.exports = findMismatch;
--- week1_basic_data_structures/2_tree_height/tree-height.js
+++ week1_basic_data_structures/2_tree_height/tree-height.js
@@ -1,158 +1,62 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    terminal: false
+});
+
+process.stdin.setEncoding('utf8');
+
+rl.once('line', () => {
+    rl.once('line', line => {
+        const parents = line.toString().split(' ').map(Number);
+
+        console.log(computeHeight(parents));
+        process.exit();
+    });
+});
+
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+    constructor(key) {
+        this.key = key;
+        this.children = [];
+        this.parent = null;
     }
 }
 
-// // depth first search
-// class Tree {
-//   constructor() {
-//     this.root = null
-//   }
-//     insertNode(root, newNode) {
-//       // new node
-//       // if we have an empty node
-//       if(!this.root) return this.root = newNode
+function computeHeight(parents) {
+    const nodes = parents.map((parent, idx) => new Node(idx));
+    const root = nodes[parents.indexOf(-1)];
+    const stack = [];
 
-//       // if the value is less then root value
-//       if(root.data < newNode.data) {
-//         // if no left child make the new node the new left child
-//         if(!root.left) {
-//           return root.left = newNode
-//         } 
-//         return this.insertNode(this.root.left)
-//       }
+    nodes.forEach((node, idx) => {
+        const parentIndex = parents[idx];
+        const parent = parentIndex >= 0 ? nodes[parentIndex] : null;
 
-//       // if the value is greater then the root
-//       else {
-//         // if no right child => make new node the new right child
-//         if(!root.right) return root.right = newNode;
-//         return this.insertNode(this.root.right)
-//       }
-//       return this.root
-//     }
-
-
-//     getHeight(node, h) {
-//       let height = h || 0;
-//       console.log(node)
-//       if(node.left) {
-//           height += 1
-//           return this.getHeight(node.left, height)
-//       } 
-//       if (node.right) {
-//           height += 1
-//           return this.getHeight(node.right, height)
-//       }
-//       return height
-//     }
-
-//     peek() {
-//         return this.root
-//     }
-// }
-
-
-class BinarySearchTree {
-  constructor() {
-    //initially root is null
-
-    this.root = null;
-  }
-
-  insertNumberNode(data, left = null, right = null) {
-    //creating a Node
-    //data we pass will act as individual parent Node
-    //left and right are subtrees
-    let Node = {
-      data,
-      left,
-      right
-    };
-    //suppose currentNumberNode as a parent node
-    //current Num Node value decides position of next value
-    //if it goes to left subtree or right subtree
-    let currentNumberNode;
-
-    if (!this.root) {
-      //if its not a root make it one by passing a Node
-      this.root = Node;
-    } else {
-      //and if its a root now, assign it to currentNumberNode
-      currentNumberNode = this.root;
-      while (currentNumberNode) {
-        //if data is smaller than cuurent data, send it in left subtree
-        if (data < currentNumberNode.data) {
-          //if current num node don't have Node properties
-          //we will assign it node properties
-          if (!currentNumberNode.left) {
-            currentNumberNode.left = Node;
-            break;
-          } else {
-            //if it has node properties and it is sent by root to left
-            //we will make it a left node because it is smaller than root node
-            currentNumberNode = currentNumberNode.left;
-          }
-          //if data is larger than cuurent data, send it in right subtree
-        } else if (data > currentNumberNode.data) {
-          //if current num node don't have Node properties
-          //we will assign it node properties
-          if (!currentNumberNode.right) {
-            currentNumberNode.right = Node;
-            break;
-          } else {
-            //if it has node properties and it is sent by root to right
-            //we will make it a right node because it is larger than root node
-            currentNumberNode = currentNumberNode.right;
-          }
-        } else {
-          console.log("Try Different Value");
-          break;
+        if (parent) {
+            node.parent = parent;
+            parent.children.push(node);
         }
-      }
+    });
+
+    let height = new Map();
+    let res = 1;
+
+    height.set(root, 1);
+    stack.push(...root.children);
+
+    while (stack.length) {
+        const node = stack.pop();
+        let h = height.get(node.parent);
+
+        if (node.children.length) {
+            height.set(node, ++h);
+        }
+
+        res = Math.max(res, h);
+        stack.push(...node.children);
     }
-  }
-  getHeight(node, h) {
-      let height = h || 0;
-      // console.log(node)
-      if(node.left) {
-          height += 1
-          return this.getHeight(node.left, height)
-      } 
-      if (node.right) {
-          height += 1
-          return this.getHeight(node.right, height)
-      }
-      return height
-    }
-    peek() {
-      return this.root
-    }
+
+    return res + 1;
 }
 
-// const n2 = {
-//   data: 8,
-//   left: 4
-// }
-// const n1 = {
-//   data: 8,
-//   left: n2
-// }
-// const root = {
-//   data: 10,
-//   left: n1
-// }
-
-const tree = new BinarySearchTree
-
-const addedValues = [1, 20, 10, 8].map(num => {
-  return tree.insertNumberNode(num)
-})
-const result = tree.getHeight(tree.peek())
-console.log(result)
-
-
-// const result = tree.getHeight(root)
-// console.log(result)
+module.exports = computeHeight;

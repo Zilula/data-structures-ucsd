68a84af9c9ab780c83a82a26bbff3b1445378e09
Lance Owen Merrill <lanceomerrill@gmail.com>
2020-04-27 17:48:47 -0700

    almost working hashmap with chaining

--- week3_hash_tables/2_hash_chains/hash_chains.js
+++ week3_hash_tables/2_hash_chains/hash_chains.js
@@ -9,79 +9,99 @@ const rl = readline.createInterface({
 
 process.stdin.setEncoding('utf8');
 
-const prime = 1000000007;
-const multiplier = 263;
 
 class HashMap {
-  constructor(size = 0) {
+  constructor(size, multiplier, prime) {
     this.map = new Array(size);
+    this.multiplier = multiplier;
+    this.prime = prime;
+    this.size = size;
   }
 
   // eslint-disable-next-line class-methods-use-this
   cash(str) {
-    let total = 0;
-    for (let i = 0; i < str.length; i += 1) {
-      total += (total + multiplier + str.charAt(i)) % prime;
+    let res = 0;
+    const len = str.length;
+
+    for (let i = len; i--;) {
+      res = (res * this.multiplier + str[i].charCodeAt(0)) % this.prime;
     }
-    return total;
+    return res % this.size;
   }
 
   add(val) {
     const hash = this.cash(val);
-
     if (!this.map[`${hash}`]) {
       this.map[`${hash}`] = [];
     }
-    this.map[`${hash}`].push([hash, val]);
+    this.map[`${hash}`].unshift([hash, val]);
   }
 
   del(val) {
     const hash = this.cash(val);
 
-    delete this.map[`${hash}`];
+    if (this.map[`${hash}`]) {
+      let index = null;
+      for (let i = 0; i < this.map[`${hash}`].length; i += 1) {
+        const [key, v] = this.map[`${hash}`][i];
+        if (v === val) {
+          index = i;
+        }
+      }
+      this.map[`${hash}`].splice(index, 1);
+    }
   }
 
   find(val) {
     const hash = this.cash(val);
-    return this.map[`${hash}`] ? 'yes' : 'no';
+    let x = false;
+    if (this.map[`${hash}`]) {
+      for (let i = 0; i < this.map[`${hash}`].length; i += 1) {
+        const [key, v] = this.map[`${hash}`][i];
+        if (v === val) {
+          x = true;
+        }
+      }
+      return this.map[`${hash}`] ? (x ? 'yes' : 'no') : 'no';
+    }
+    return 'no';
   }
 
-  check(val) {
-    const hash = this.cash(val);
+  check(hash) {
     if (this.map[`${hash}`]) {
       const res = [];
       for (let i = 0; i < this.map[`${hash}`].length; i += 1) {
         const [key, v] = this.map[`${hash}`][i];
         res.push(v);
       }
-      return res;
+      return res.join(' ');
     }
     return ' ';
   }
 }
 
 const handleQueries = (lines) => {
-  const map = new HashMap(lines.length);
+  const map = new HashMap(5, 263, 1000000007);
 
   for (let i = 0; i < lines.length; i += 1) {
-    const [cmd, number] = lines[i].split(' ');
-    const r = map[cmd](number);
+    const [cmd, str] = lines[i].split(' ');
+    const r = map[cmd](str);
     if (r) {
       console.log(r);
     }
   }
 };
 
-
-rl.on('line', () => {
-  rl.on('line', (line) => {
-    const n = Number(line);
-    const queries = [];
+rl.once('line', (line) => {
+  const nOutput = Number(line);
+  const queries = [];
+  rl.once('line', (input) => {
+    const nInput = Number(input);
     rl.on('line', (args) => {
       queries.push(args.toString());
 
-      if (queries.length === n) {
-        handleQueries(queries);
+      if (queries.length === nInput) {
+        handleQueries(queries, nOutput);
         process.exit();
       }
     });
